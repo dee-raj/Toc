80ad19a45f821f51f80de90627a2947a1656ff69
dee-raj <dhurbaraj343sky@gmail.com>
2023-01-06 21:23:47 +0530

    Complete code for checking a number is divisible by 2 or not?

--- divideBy2.py
+++ divideBy2.py
@@ -2,13 +2,32 @@
 def q0(s,i):
   print("Q0=> ",end='')
   if i==len(s):
-    print(not final state, number is not divisible by 2")
+    print("not final state, number is not divisible by 2")
     return
-  if s[i]='0':
+  if s[i]=='0':
     q0(s,i+1)
   else:
     q2(s,i+1)
-          
+
+def q1(s,i):
+  print("Q0=> ",end='')
+  if i==len(s):
+    print("not final state, number is not divisible by 2")
+    return
+  if s[i]=='0':
+    q1(s,i+1)
+  else:
+    q2(s,i+1)
+
+def q2(s,i):
+  print("Q0=> ",end='')
+  if i==len(s):
+    print("not final state, number is not divisible by 2")
+    return
+  if s[i]=='0':
+    q1(s,i+1)
+  else:
+    q2(s,i+1)
 w='101101'
-print(f"is {w} divisible by 2 ?\n")
+print(f"is {w} divisible by 2 ?")
 q1(w,0)
